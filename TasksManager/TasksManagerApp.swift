30a9cf563a3613a49495be38787244661f47c774
Sama <samaa@blink.global>
2025-10-08 14:42:02 +0300

    Initial Commit

---
+++ TasksManager/TasksManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksManagerApp.swift
+//  TasksManager
+//
+//  Created by Samaa Soltan on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TasksManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
